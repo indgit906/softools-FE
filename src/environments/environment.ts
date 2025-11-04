810ddb50517f990a73f8e30fe190f6c41d77b38b
indgit906 <14cs055@gmail.com>
2025-11-04 13:26:21 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // API: 'https://0dabd9df4318.ngrok-free.app/softools/api',
-  API: 'http://43.205.109.19:8083/softools/api',
+  //API: 'http://43.205.109.19:8083/softools/api',
 };
